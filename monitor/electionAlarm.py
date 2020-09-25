854b7ac527a9f28521606e5dd5ee62a2f6fdb43c
kevintmax <67485702+kevintmax@users.noreply.github.com>
2020-09-25 13:37:11 +0900

    Update electionAlarm.py

--- monitor/electionAlarm.py
+++ monitor/electionAlarm.py
@@ -5,3 +5,9 @@ import time
 from time import sleep
 my_token = '1106924426:AAGC1XlEMcuEGRivP7OkJHisk45MRL3VrS4'
 bot = telegram.Bot(token = my_token)
+
+VALIDATION=subprocess.check_output("cat ~/node.operator/logs/election.log | grep -oP '(?<=VALIDATION_STATUS: )[0-9]'",shell=True);
+ELECTION=subprocess.check_output("cat ~/node.operator/logs/election.log | grep -oP '(?<=Election_STATUS: )[0-9]'",shell=True);
+
+if float(VALIDATION) =1
+  print "validating"
