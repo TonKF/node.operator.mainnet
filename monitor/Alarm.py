df319f834b72a18d62ee1944c1f1fbda47607701
kevintmax <67485702+kevintmax@users.noreply.github.com>
2020-09-25 13:45:51 +0900

    Update Alarm.py

--- monitor/Alarm.py
+++ monitor/Alarm.py
@@ -1,3 +1,5 @@
+import getpass
+print(username)
 import telegram
 import subprocess
 import psutil
@@ -6,9 +8,13 @@ from time import sleep
 my_token = '1106924426:AAGC1XlEMcuEGRivP7OkJHisk45MRL3VrS4'
 bot = telegram.Bot(token = my_token)
 
-VALIDATION=subprocess.check_output("cat ~/node.operator/logs/election.log | grep -oP '(?<=VALIDATION_STATUS: )[0-9]'",shell=True);
-ELECTION=subprocess.check_output("cat ~/node.operator/logs/election.log | grep -oP '(?<=ELECTION_STATUS: )[0-9]'",shell=True);
-
+VALIDATION = subprocess.check_output("cat ~/node.operator/logs/election.log | grep -oP '(?<=VALIDATION_STATUS: )[0-9]'",shell=True);
+ELECTION = subprocess.check_output("cat ~/node.operator/logs/election.log | grep -oP '(?<=ELECTION_STATUS: )[0-9]'",shell=True);
+USERNAME = getpass.getuser()
 print VALIDATION
+print ELECTION
+print USERNAME
 if int(VALIDATION) == 1:
-  print "validating"
+  print str(USERNAME)+"validating"
+if int(ELECTION) == 1:
+  print str(username)+"in Election"
