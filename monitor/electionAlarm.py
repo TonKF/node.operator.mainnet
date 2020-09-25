3deb8e976d3c4b80517f951fe4b73c36138239f4
kevintmax <67485702+kevintmax@users.noreply.github.com>
2020-09-25 13:29:39 +0900

    Create electionAlarm.py

---
+++ monitor/electionAlarm.py
@@ -0,0 +1,7 @@
+import telegram
+import subprocess
+import psutil
+import time
+from time import sleep
+my_token = '1106924426:AAGC1XlEMcuEGRivP7OkJHisk45MRL3VrS4'
+bot = telegram.Bot(token = my_token)
