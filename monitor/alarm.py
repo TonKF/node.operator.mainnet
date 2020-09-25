21dc0c405cad3ea69dab82268ec6fa6f3b262a2a
kevintmax <67485702+kevintmax@users.noreply.github.com>
2020-09-25 22:09:11 +0900

    Update alarm.py

--- monitor/alarm.py
+++ monitor/alarm.py
@@ -11,6 +11,7 @@ HOSTNAME = socket.gethostname()
 VALIDATION = subprocess.check_output("cat ~/node.operator/logs/election.log | grep -oP '(?<=VALIDATION_STATUS: )[0-9]'",shell=True);
 ELECTION = subprocess.check_output("cat ~/node.operator/logs/election.log | grep -oP '(?<=ELECTION_STATUS: )[0-9]'",shell=True);
 SYNC=subprocess.check_output("~/net.ton.dev/scripts/check_node_sync_status.sh | grep TIME_DIFF | awk '{print $4}' | tr '[:upper:]' '[:lower:]'",shell=True);
+SYNCOFF=subprocess.check_output("~/net.ton.dev/scripts/check_node_sync_status.sh | grep error"),shell=True);
 
 SYNCalarm = "-100"
 SYNCalarm1 = 0
@@ -26,3 +27,6 @@ if int(SYNC) < int(SYNCalarm):
   bot.sendMessage(chat_id='-1001416401295', text=str(HOSTNAME)+" SYNC off, SYNC:"+str(SYNC));
 if int(SYNC) > int(SYNCalarm1):
   bot.sendMessage(chat_id='-1001416401295', text=str(HOSTNAME)+" SYNC off, SYNC:"+str(SYNC));
+if len(SYNCOFF) == 1:
+  bot.sendMessage(chat_id='-1001416401295', text=str(HOSTNAME)+" SYNC error");
+  
