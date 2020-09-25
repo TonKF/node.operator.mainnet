fa4f74cb394368c94acb92704199c5bd6a422290
kevintmax <67485702+kevintmax@users.noreply.github.com>
2020-09-25 13:40:44 +0900

    Update Alarm.py

--- monitor/Alarm.py
+++ monitor/Alarm.py
@@ -9,5 +9,5 @@ bot = telegram.Bot(token = my_token)
 VALIDATION=subprocess.check_output("cat ~/node.operator/logs/election.log | grep -oP '(?<=VALIDATION_STATUS: )[0-9]'",shell=True);
 ELECTION=subprocess.check_output("cat ~/node.operator/logs/election.log | grep -oP '(?<=ELECTION_STATUS: )[0-9]'",shell=True);
 
-if float(VALIDATION) == "1"
+if float(VALIDATION) == "1":
   print "validating"
