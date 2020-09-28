eafb592d7cf44fa8f301e871f8373496034efc1c
kevintmax <67485702+kevintmax@users.noreply.github.com>
2020-09-28 15:03:42 +0900

    Update validatorBuild.sh

--- scripts/validatorBuild.sh
+++ scripts/validatorBuild.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo apt update -y
 sudo apt upgrade -y
 
@@ -10,12 +9,12 @@ sudo apt install bc -y
 cd ${NODE_OPERATOR_SCRIPTS_DIR} && . ${NODE_OPERATOR_CONFIGS_DIR}/env.sh
 
 #install node
-cd && git clone https://github.com/tonlabs/net.ton.dev.git
-mv ~/net.ton.dev ~/main.ton.dev
-
+cd && git clone https://github.com/tonlabs/main.ton.dev.git
 cd ${SCRIPTS_DIR}
 . ./env.sh
 ./build.sh
-export NETWORK_TYPE=net.ton.dev
 ./setup.sh
 ./run.sh
+rm validator_msig.sh
+wget https://raw.githubusercontent.com/tonlabs/net.ton.dev/master/scripts/validator_msig.sh
+chmod +x validator_msig.sh
