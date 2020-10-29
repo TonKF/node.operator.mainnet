a2279047c3396e0d7c0efffdf511a54bb8352fdd
kevintmax <67485702+kevintmax@users.noreply.github.com>
2020-10-29 16:05:20 +0900

    Update alarm.py

--- monitor/alarm.py
+++ monitor/alarm.py
@@ -8,9 +8,9 @@ my_token = '1106924426:AAGC1XlEMcuEGRivP7OkJHisk45MRL3VrS4'
 bot = telegram.Bot(token = my_token)
 
 HOSTNAME = socket.gethostname()
-VALIDATION = subprocess.check_output("cat ~/node.operator/logs/election.log | grep -oP '(?<=VALIDATION_STATUS: )[0-9]'",shell=True);
+VALIDATION = subprocess.check_output("cat ~/node.operator/logs/election.log | grep -oP '(?<=VALIDATION_STATUS: )[0-9]'",shell=True,);
 ELECTION = subprocess.check_output("cat ~/node.operator/logs/election.log | grep -oP '(?<=ELECTION_STATUS: )[0-9]'",shell=True);
-SYNC=subprocess.check_output("~/main.ton.dev/scripts/check_node_sync_status.sh | grep TIME_DIFF | awk '{print $4}' | tr '[:upper:]' '[:lower:]'",shell=True);
+SYNC=subprocess.check_output("~/net.ton.dev/scripts/check_node_sync_status.sh | grep TIME_DIFF | awk '{print $4}' | tr '[:upper:]' '[:lower:]'",shell=True);
 
 
 SYNCalarm = "-100"
@@ -27,4 +27,3 @@ if int(SYNC) < int(SYNCalarm):
   bot.sendMessage(chat_id='-1001416401295', text=str(HOSTNAME)+" SYNC off, SYNC:"+str(SYNC));
 if int(SYNC) > int(SYNCalarm1):
   bot.sendMessage(chat_id='-1001416401295', text=str(HOSTNAME)+" SYNC off, SYNC:"+str(SYNC));
- 
